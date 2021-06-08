c12bd28f99878324bcd91860e952546d5407e6d8
tapasyat <tapasya@singular.net>
2021-06-08 14:17:53 +0530

    checking

--- Package.swift
+++ Package.swift
@@ -6,9 +6,6 @@ import PackageDescription
 let package = Package(
     name: "Singular",
     products: [
-        .library(
-            name: "Singular",
-            targets: ["Singular"]),
     ],
     dependencies: [
     ],
